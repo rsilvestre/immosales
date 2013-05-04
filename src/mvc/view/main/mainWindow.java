5eef9d53143405757da000fbc4f1959b861cb606
Michaël Silvestre <willtard@gmail.com>
2013-05-04 11:28:28 +0200

    Ajout d'une fenêtre principale

---
+++ src/mvc/view/main/mainWindow.java
@@ -0,0 +1,11 @@
+package mvc.view.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelsilvestre
+ * Date: 4/05/13
+ * Time: 09:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class mainWindow {
+}
