5e412fa8a5c408cf793fb8ba17934e8b21a22fc4
Michaël Silvestre <willtard@gmail.com>
2013-05-04 11:28:28 +0200

    Ajout d'une fenêtre principale

---
+++ src/mvc/view/identity/PersonWindow.java
@@ -0,0 +1,11 @@
+package mvc.view.identity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelsilvestre
+ * Date: 4/05/13
+ * Time: 00:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class PersonWindow {
+}
