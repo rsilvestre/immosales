051835fe69bb0d9ba67fb74fc7844407269b679e
Michaël Silvestre <willtard@gmail.com>
2013-05-12 09:51:17 +0200

    Structure nouveaux objets

---
+++ src/mvc/controller/owner/OwnerController.java
@@ -0,0 +1,11 @@
+package mvc.controller.owner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelsilvestre
+ * Date: 11/05/13
+ * Time: 14:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class OwnerController {
+}
---
+++ src/mvc/model/owner/OwnerPanelModel.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2013. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package mvc.model.owner;
+
+import com.dmurph.mvc.model.AbstractModel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelsilvestre
+ * Date: 11/05/13
+ * Time: 14:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class OwnerPanelModel extends AbstractModel {
+
+}
